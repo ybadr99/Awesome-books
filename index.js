1716279510bd1c62f869b0c716a2716c8f3431dc
ybadr99 <yousefbadr909@gmail.com>
2023-05-02 18:40:54 +0200

    fix linters:

--- index.js
+++ index.js
@@ -1,52 +1,49 @@
-import Book from "./modules/book.js";
-import { DateTime } from "./modules/luxon.js";
+import Book from './modules/book.js';
+import { DateTime } from './modules/luxon.js';
 
-const form = document.querySelector("#form");
-const title = document.querySelector("#title");
-const author = document.querySelector("#author");
-const navLinks = document.querySelectorAll("li");
-const sections = document.querySelectorAll("section");
-
-
-
-Book.renderBooks();
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  Book.add(title.value, author.value);
-  title.value = "";
-  author.value = "";
-  Book.renderBooks();
-  showSection("section-list");
-});
+const form = document.querySelector('#form');
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const navLinks = document.querySelectorAll('li');
+const sections = document.querySelectorAll('section');
 
 const showSection = (id) => {
   navLinks.forEach((li) => {
     if (id === li.id) {
-      li.classList.add("active");
+      li.classList.add('active');
     } else {
-      li.classList.remove("active");
+      li.classList.remove('active');
     }
   });
 
   sections.forEach((sec) => {
     if (sec.classList.value !== id) {
-      sec.style.setProperty("display", "none");
+      sec.style.setProperty('display', 'none');
     } else {
-      sec.style.setProperty("display", "block");
+      sec.style.setProperty('display', 'block');
     }
   });
 };
 
+Book.renderBooks();
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  Book.add(title.value, author.value);
+  title.value = '';
+  author.value = '';
+  Book.renderBooks();
+  showSection('section-list');
+});
+
 navLinks.forEach((li) => {
-  li.addEventListener("click", () => {
+  li.addEventListener('click', () => {
     showSection(li.id);
   });
 });
 
-showSection("section-list");
-
+showSection('section-list');
 
 // Sate
 const now = new Date();
